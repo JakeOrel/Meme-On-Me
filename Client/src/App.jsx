3b3cdc91ee8b231fdb4d9a19418f4a5b8b018235
Jacob Orel <jakeorel@gmail.com>
2020-06-06 17:27:57 -0500

    created styled comps

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+import axios from 'axios';
+import Tiles from './Tiles';
+import { Header, TileHolder } from './styles';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      entries: [{ user: 'johnny bravo', ups: 100, thumbnail: 'https://b.thumbs.redditmedia.com/pyRe8UVjyJrmihhw2B5DvBEh5sElYaEOXrbNTswqshU.jpg' }],
+    };
   }
 
+  // componentDidMount() {
+  //   this.getPosts();
+  // }
+
+  // getPosts() {
+  //   let uName = 'home';
+  //   const url = window.location.href;
+  //   const query = url.slice(url.indexOf('?'));
+  //   const params = new URLSearchParams(query);
+  //   if (params.get('user') !== null) {
+  //     uName = params.get('user');
+  //   }
+  //   axios.get(`/${uName}`)
+  //     .then((data) => {
+  //       this.setState({
+  //         entries: data,
+  //       });
+  //     });
+  // }
+
   render() {
+    const { entries } = this.state;
     return (
-      <h1> Meme On Me</h1>
+      <div>
+        <Header>Meme On Me</Header>
+        <TileHolder>
+          <Tiles entries={entries} />
+        </TileHolder>
+      </div>
     );
   }
 }
--- Client/src/styles.jsx
+++ Client/src/styles.jsx
@@ -1,1 +1,34 @@
+/* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
+
+export const Header = styled.div`
+  color: #be79df;
+  font-size: 28px;
+  border: 1px solid grey;
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0.1);
+`;
+
+export const TileHolder = styled.div`
+  margin-top: 10%;
+  margin-left: 30vw;
+  width: 45vw;
+`;
+
+export const UserName = styled.h5`
+  color: #be79df;
+  font-size: 24px;
+`;
+
+export const Tile = styled.div`
+  background-color: rgba(255, 255, 255, 0.08);
+  border-radius: 5%;
+`;
+
+export const Up = styled.button`
+  background-color: #00bdaa;
+  border: none;
+  height: 40px;
+  width: 40px;
+  border-radius: 20px;
+`;
