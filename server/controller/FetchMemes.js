23802e5c09c6adb548f4e1ba469b43f9b8b8dcb8
Jacob Orel <jakeorel@gmail.com>
2020-06-06 19:11:29 -0500

    changed api

--- server/controller/FetchMemes.js
+++ server/controller/FetchMemes.js
@@ -1,9 +1,12 @@
-const reddit = require('../models/searchReddit');
+const reddit = require('../models/searchImg');
 
 exports.loadPage = (req, res) => {
   const { searchTerm, sortBy, searchLimit } = req.body;
 
   reddit.loadPage(searchTerm, searchLimit, sortBy)
-    .then((data) => res.send(data.data.data.children))
+    .then((data) => {
+      const entries = data.data.data;
+      res.send(entries.memes);
+    })
     .catch((err) => res.send(err));
 };
